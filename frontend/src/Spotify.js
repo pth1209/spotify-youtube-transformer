e2a1007e39a2359dd79a7eec726338e55aca1873
yoyoconnor <111534505+yoyoconnor@users.noreply.github.com>
2023-05-29 21:28:19 -0500

    barbones ui done

--- frontend/src/Spotify.js
+++ frontend/src/Spotify.js
@@ -1,8 +1,33 @@
 import './App.css';
+import React, {useState} from "react"
+import axios from "axios"
 
 function Spotify() {
-    return(
-        <textarea className='link' placeholder='Paste your Spotify link here'></textarea>
+   
+        const [playlistUrl, setPlaylistUrl] = useState("")
+
+        const handleInputChange = (event) => {
+            setPlaylistUrl(event.target.value)
+        }
+      
+        const sendUrlToBackend = async () => {
+          console.log(playlistUrl.split('/playlist/')[1])
+          try {
+            const response = await axios.post('/api/get-song-from-playlist', {playlistUrl})
+            console.log(response.data)
+          } catch (error)  {
+            console.log(error)
+          }
+        }
+        return (
+        <div className="App">
+      <header className="App-header">
+        <input type = "text" name = "playlistLink" onChange = {handleInputChange}/>
+        <button className='button' onClick={sendUrlToBackend}>Submit</button>
+      </header>
+    </div>
+
+      
     );
 }
 
--- frontend/src/Youtube.js
+++ frontend/src/Youtube.js
@@ -1,9 +1,34 @@
-
 import './App.css';
+import React, {useState} from "react"
+import axios from "axios"
 
-function Youtube() {
-    return(
-        <textarea className='link' placeholder='Paste your Youtube link here'></textarea>
+function YouTube() {
+   
+        const [playlistUrl, setPlaylistUrl] = useState("")
+
+        const handleInputChange = (event) => {
+            setPlaylistUrl(event.target.value)
+        }
+      
+        const sendUrlToBackend = async () => {
+          console.log(playlistUrl.split('/playlist/')[1])
+          try {
+            const response = await axios.post('/api/get-song-from-playlist', {playlistUrl})
+            console.log(response.data)
+          } catch (error)  {
+            console.log(error)
+          }
+        }
+        return (
+        <div className="App">
+      <header className="App-header">
+        <input type = "text" name = "playlistLink" onChange = {handleInputChange}/>
+        <button className='button' onClick={sendUrlToBackend}>Submit</button>
+      </header>
+    </div>
+
+      
     );
 }
-export default Youtube;
+
+export default YouTube;
