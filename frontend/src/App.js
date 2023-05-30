db9fded2be920910800281de06f2b79db4eb366a
pth1209 <113861167+pth1209@users.noreply.github.com>
2023-05-29 22:35:06 -0500

    update routing and ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,53 +9,50 @@ import youtubeLogo from "./youtube.png"
 
 function App() {
   
-  const [playlistUrl, setPlaylistUrl] = useState("")
+//   const [playlistUrl, setPlaylistUrl] = useState("")
 
-  const handleInputChange = (event) => {
-      setPlaylistUrl(event.target.value)
-      if (event.target.value.includes('spotify')) {
-        console.log("spotify")
-        SpotyifyFirst()
+//   const handleInputChange = (event) => {
+//       setPlaylistUrl(event.target.value)
+//       if (event.target.value.includes('spotify')) {
+//         console.log("spotify")
+//         SpotyifyFirst()
 
-      } else if (event.target.value.includes('youtube')) {
-        console.log("youtube")
-        YouTubeFirst()
-      }
-  }
+//       } else if (event.target.value.includes('youtube')) {
+//         console.log("youtube")
+//         YouTubeFirst()
+//       }
+//   }
 
-  const sendUrlToBackend = async () => {
-    console.log(playlistUrl.split('/playlist/')[1])
-    try {
-      const response = await axios.post('/api/get-song-from-playlist', {playlistUrl})
-      console.log(response.data)
-    } catch (error)  {
-      console.log(error)
-    }
-  }
+//   const sendUrlToBackend = async () => {
+//     console.log(playlistUrl.split('/playlist/')[1])
+//     try {
+//       const response = await axios.post('/api/get-song-from-playlist', {playlistUrl})
+//       console.log(response.data)
+//     } catch (error)  {
+//       console.log(error)
+//     }
+//   }
 
-  const [isSwapped, setIsSwapped] = useState(false);
+//   const [isSwapped, setIsSwapped] = useState(false);
 
-  const swapLogos = () => {
-    setIsSwapped(!isSwapped);
-  };
-  const SpotyifyFirst = () => {
-    if(isSwapped){
-        swapLogos()
-    }
-}
-const YouTubeFirst = () => {
-    if(!isSwapped){
-        swapLogos()
-    }
-}
+//   const swapLogos = () => {
+//     setIsSwapped(!isSwapped);
+//   };
+//   const SpotyifyFirst = () => {
+//     if(isSwapped){
+//         swapLogos()
+//     }
+// }
+// const YouTubeFirst = () => {
+//     if(!isSwapped){
+//         swapLogos()
+//     }
+// }
 
   return (
     <div className="App">
       <header className="App-header">
         <Logo />
-        <input type = "text" name = "playlistLink" onChange = {handleInputChange}/>
-        <button className='button' onClick={sendUrlToBackend}>Submit</button>
-      {/* <button className='button' onclick="YTFirst()">Submit</button> */}
       </header>
     </div>
   );
--- frontend/src/Logo.js
+++ frontend/src/Logo.js
@@ -3,21 +3,20 @@ import spotifyLogo from './spotify_converter/spotify.png';
 import arrow from './arrow.png';
 import youtubeLogo from './youtube.png';
 import { useState } from 'react';
-
-
+import {Link} from "react-router-dom";
 
 const LogoRow = ({ isSwapped, swapLogos }) => {
 
 
   return (
     <div className="logo-row">
-          <a href="/youtube">
+          <Link to ="/youtube">
           <img src={youtubeLogo} alt="YouTube Logo" className="ylogo" />
-          </a>
+          </Link>
           <p>OR</p>
-          <a href="/spotify">
+          <Link to ="/spotify">
           <img src={spotifyLogo} alt="Spotify Logo" className="slogo" />
-          </a>
+          </Link>
     </div>
   );
 };
