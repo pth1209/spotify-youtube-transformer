3980e2462b49bc8b4e87ae71dc3c70f5f976c55b
Elijah Malabuyo <elijahbmalabuyo@gmail.com>
2023-06-01 13:19:57 -0700

    working component for front to back

--- backend/Controllers/SpotifyControllers.js
+++ backend/Controllers/SpotifyControllers.js
@@ -1,59 +1,64 @@
 const { google } = require("googleapis");
-const axios = require('axios');
+const SpotifyWebApi = require("spotify-web-api-node");
+const axios = require("axios");
 
 const createYoutubePlaylist = async (req, res) => {
-  const tokenId = req.body.accessToken;
+  const tokenId = req.headers.authorization.split(" ")[1];
   const playlistUrl = req.body.playlistUrl;
   const spotifyClientId = process.env.SPOTIFY_CLIENT_ID;
   const spotifyClientSecret = process.env.SPOTIFY_CLIENT_SECRET;
 
   try {
-    const playlistId = playlistUrl.split('/playlist/')[1];
-
+    const playlistId = playlistUrl.split("/playlist/")[1];
+    console.log(playlistId)
+    console.log(tokenId)
     // Get Spotify access token
     const authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      method: 'post',
+      url: "https://accounts.spotify.com/api/token",
+      method: "post",
       headers: {
-        'Authorization': 'Basic ' + Buffer.from(spotifyClientId + ':' + spotifyClientSecret).toString('base64'),
-        'Content-Type': 'application/x-www-form-urlencoded'
+        Authorization:
+          "Basic " +
+          Buffer.from(
+            spotifyClientId + ":" + spotifyClientSecret
+          ).toString("base64"),
+        "Content-Type": "application/x-www-form-urlencoded",
       },
       params: {
-        grant_type: 'client_credentials'
-      }
+        grant_type: "client_credentials",
+      },
     };
 
     const { data } = await axios(authOptions);
     const accessToken = data.access_token;
 
-    // Use the access token to make requests to Spotify Web API
-    const playlistResponse = await axios.get(`https://api.spotify.com/v1/playlists/${playlistId}`, {
-      headers: {
-        'Authorization': 'Bearer ' + accessToken
-      }
-    });
+    // Initialize Spotify Web API client
+    const spotifyApi = new SpotifyWebApi();
+    spotifyApi.setAccessToken(accessToken);
 
-    const playlistData = playlistResponse.data;
+    // Get the playlist details from Spotify
+    const { body: playlistData } = await spotifyApi.getPlaylist(playlistId);
+
     const name = playlistData.name;
-    const tracks = playlistData.tracks;
+    const tracks = playlistData.tracks.items;
 
     // Gets the track names and artists from each song
-    const trackList = tracks.items.map((item) => {
+    const trackList = tracks.map((item) => {
       const track = item.track;
       return {
         title: track.name,
-        artist: track.artists.map((artist) => artist.name).join(', '),
+        artist: track.artists.map((artist) => artist.name).join(", "),
       };
     });
 
     const oauth2Client = new google.auth.OAuth2();
     oauth2Client.setCredentials({ access_token: tokenId });
 
-    const youtube = google.youtube({ version: 'v3', auth: oauth2Client });
+    const youtube = google.youtube({ version: "v3", auth: oauth2Client });
     const playlistTitle = `${name} on Spotify`;
 
     const { data: playlistInsertData } = await youtube.playlists.insert({
-      part: 'snippet',
+      part: "snippet",
       requestBody: {
         snippet: {
           title: playlistTitle,
@@ -62,37 +67,41 @@ const createYoutubePlaylist = async (req, res) => {
     });
 
     const youtubePlaylistId = playlistInsertData.id;
-    const videoIds = await Promise.all(trackList.map(
-      (track) => searchYouTubeVideoId(track.title, track.artist, youtube)
-    ));
+    const videoIds = await Promise.all(
+      trackList.map((track) =>
+        searchYouTubeVideoId(track.title, track.artist, youtube)
+      )
+    );
 
-    await Promise.all(videoIds.map((videoId) => {
-      return youtube.playlistItems.insert({
-        part: 'snippet',
-        requestBody: {
-          snippet: {
-            playlistId: youtubePlaylistId,
-            resourceId: {
-              kind: 'youtube#video',
-              videoId,
+    await Promise.all(
+      videoIds.map((videoId) =>
+        youtube.playlistItems.insert({
+          part: "snippet",
+          requestBody: {
+            snippet: {
+              playlistId: youtubePlaylistId,
+              resourceId: {
+                kind: "youtube#video",
+                videoId,
+              },
             },
           },
-        },
-      });
-    }));
+        })
+      )
+    );
 
     res.json(playlistInsertData);
   } catch (error) {
-    console.error('Error creating playlist:', error);
-    res.status(500).json({ error: 'Failed to create playlist' });
+    console.error("Error creating playlist:", error);
+    res.status(500).json({ error: "Failed to create playlist" });
   }
 };
 
 const searchYouTubeVideoId = async (title, artist, youtube) => {
   const { data } = await youtube.search.list({
-    part: 'snippet',
+    part: "snippet",
     q: `${title} ${artist}`,
-    type: 'video',
+    type: "video",
   });
 
   return data.items[0].id.videoId;
--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,20 @@ const router = require("./Routes/routes.js");
 
 app.use(express.json());
 
-app.use('/api/create-youtube-playlist', router); // Mount the router at the '/api' base URL path
+// app.use('/api/create-youtube-playlist', router);// Mount the router at the '/api' base URL path
+
+
+app.post('/api/create-youtube-playlist', (req, res) => {
+    try {
+  const {playlistUrl} =  req.body;
+  const accessToken = req.headers.authorization.split(' ')[1];
+  console.log(playlistUrl)
+  console.log(accessToken);
+    } catch (error) {
+      res.status(500).json({error: error})
+    }
+  })
+
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
